4aed5d8de5713492619f70b030c1f6359f12f01d
Luboshh <luboshhh@gmail.com>
2022-12-04 10:43:09 +0100

    Created RoomForm and RoomCreateView

--- PythonRemoteCZ9/asgi.py
+++ PythonRemoteCZ9/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for chatterbox_project project.
+ASGI config for PythonRemoteCZ9 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatterbox_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PythonRemoteCZ9.settings')
 
 application = get_asgi_application()
